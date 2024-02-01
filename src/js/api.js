abbd767428961a607ee96c7162340f65f8ebbc42
Pranav Rajesh Jadhav <iamoutput09@gmail.com>
2024-02-01 11:32:09 +0530

    🔗 all api configuration

--- src/js/api.js
+++ src/js/api.js
@@ -4,3 +4,39 @@
  * @copyright jrpranav 2024 All rights reserved
  * @author jrpranav <pranav@jrpranav.com>
  */
+
+"use strict";
+
+const api_Key = "e63f5b86dca5a14d0c33efcbeebb6977";
+
+/**
+ * Fetch data from server
+ * @param {string} URL API url
+ * @param {function} callback callback
+ */
+export const fetchData = function (URL, callback) {
+  fetch(`${URL}&appid=${api_Key}`)
+    .then((res) => res.json)
+    .then((data) => callback(data));
+};
+
+export const url = {
+  currentWeather(lat, lon) {
+    return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`;
+  },
+  forecast(lat, lon) {
+    return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`;
+  },
+  airPollution(lat, lon) {
+    return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`;
+  },
+  reverseGeo(lat, lon) {
+    return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`;
+  },
+  /**
+   * @param {string} query Search query e.g.: "Pune","Mumbai"
+   */
+  geo(query) {
+    return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`;
+  },
+};
