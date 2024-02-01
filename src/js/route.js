a44cb078eb4966ccd28bd9a4a578222c44cb841b
Pranav Rajesh Jadhav <iamoutput09@gmail.com>
2024-02-01 12:53:02 +0530

    🛣  all routes config set

--- src/js/route.js
+++ src/js/route.js
@@ -4,3 +4,53 @@
  * @copyright jrpranav 2024 All rights reserved
  * @author jrpranav <pranav@jrpranav.com>
  */
+
+"use strict";
+
+import { updateWeather, error404 } from "./app.js";
+
+const defaultLocation = "#/weather?lat=26.7951906&lon=82.1945943"; // Shree Ram Janmabhoomi Mandir
+
+const currentLocation = function () {
+  window.navigator.geolocation.getCurrentPosition(
+    (res) => {
+      const { latitude, longitude } = res.coords;
+
+      updateWeather(`lat=${latitude}`, `lon=${longitude}`);
+    },
+    (err) => {
+      window.location.hash = defaultLocation;
+    }
+  );
+};
+
+/**
+ * @param {string} query Searched query
+ */
+const searchedLocation = (query) => updateWeather(...query.split("&"));
+// updateWeather("lat=26.7951906", "lon=82.1945943")
+
+const routes = new Map([
+  ["/current-location", currentLocation],
+  ["/weather", searchedLocation],
+]);
+
+const checkHash = function () {
+  const requestURL = window.location.hash.slice(1);
+
+  const [route, query] = requestURL.includes
+    ? requestURL.split("?")
+    : [requestURL];
+
+  routes.get(route) ? routes.get(route)(query) : error404();
+};
+
+window.addEventListener("hashchange", checkHash);
+
+window.addEventListener("load", function () {
+  if (!window.location.hash) {
+    window.location.hash = "#/current-location";
+  } else {
+    checkHash();
+  }
+});
