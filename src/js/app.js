675e38a76ca6d6e6d664486274cd10dbbf0380b0
Pranav Rajesh Jadhav <iamoutput09@gmail.com>
2024-02-02 20:00:52 +0530

    feat: ✨ 5 day forecast added

--- src/js/app.js
+++ src/js/app.js
@@ -103,9 +103,9 @@ const errorContent = document.querySelector("[data-error-content]");
  * @param {number} lon Longitude
  */
 export const updateWeather = function (lat, lon) {
-  // loading.style.display = "grid";
-  // container.style.overflowY = "hidden";
-  // container.classList.remove("fade-in");
+  loading.style.display = "grid";
+  container.style.overflowY = "hidden";
+  container.classList.remove("fade-in");
   errorContent.style.display = "none";
 
   const currentWeatherSection = document.querySelector(
@@ -385,6 +385,56 @@ export const updateWeather = function (lat, lon) {
       /**
        * 5 Day Forecast Section
        */
+      forecastSection.innerHTML = `
+      <h2 class="title-2" id="forecast-label">5 Days Forecast</h2>
+
+      <div class="card card-lg forecast-card">
+        <ul data-forecast-list></ul>
+      </div>
+      `;
+
+      let len = forecastList.length;
+      for (let i = 7; i < len; i += 8) {
+        const {
+          main: { temp_max },
+          weather,
+          dt_txt,
+        } = forecastList[i];
+
+        const [{ icon, description }] = weather;
+        const date = new Date(dt_txt);
+
+        const li = document.createElement("li");
+        li.classList.add("card-item");
+
+        li.innerHTML = `
+        <div class="icon-wrapper">
+        <img
+          src="./src/assets/weather_icons/${icon}.png"
+          width="36"
+          height="36"
+          alt="${description}"
+          class="weather-icon"
+          title="${description}"
+        />
+
+        <span class="span">
+          <p class="title-2">${parseInt(temp_max)}&deg;</p>
+        </span>
+      </div>
+
+      <p class="label-1">${date.getDate()} ${
+          module.monthNames[date.getUTCMonth()]
+        }</p>
+      <p class="label-1">${module.weekDayNames[date.getUTCDay()]}</p>
+        `;
+
+        forecastSection.querySelector("[data-forecast-list]").appendChild(li);
+      }
+
+      loading.style.display = "none";
+      container.style.overflowY = "overlay";
+      container.classList.add("fade-in");
     });
   });
 };
