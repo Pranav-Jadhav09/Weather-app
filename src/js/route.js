0cee311e21bbc711c9e23754caabe108c8e89ccd
Pranav Rajesh Jadhav <iamoutput09@gmail.com>
2024-02-06 11:36:12 +0530

    updated routes

--- src/js/route.js
+++ src/js/route.js
@@ -9,47 +9,59 @@
 
 import { updateWeather, error404 } from "./app.js";
 
-const defaultLocation = "#/weather?lat=26.7951906&lon=82.1945943"; // Shree Ram Janmabhoomi Mandir
+const defaultLoaction = "#/weather?lat=26.7951906&lon=82.1945943"; // Shree Ram Janmabhoomi Mandir
 
-const currentLocation = function () {
+const approximatelocation = async () => {
+  try {
+    let ipResponse = await fetch("https://api.ipify.org/?format=json");
+    let ipData = await ipResponse.json();
+    let ip = ipData.ip;
+    let locationResponse = await fetch(
+      `https://ipinfo.io/${ip}?token=c177813f87d9fa`
+    );
+    let locationData = await locationResponse.json();
+    const [latitude, longitude] = locationData.loc.split(",");
+    updateWeather(`lat=${latitude}`, `lon=${longitude}`);
+    window.location.hash = `#/approximatelocation`;
+  } catch (error) {
+    console.error("Error fetching location data:", error);
+  }
+};
+
+const currentLoction = () => {
   window.navigator.geolocation.getCurrentPosition(
     (res) => {
       const { latitude, longitude } = res.coords;
-
       updateWeather(`lat=${latitude}`, `lon=${longitude}`);
     },
     (err) => {
-      window.location.hash = defaultLocation;
+      approximatelocation();
     }
   );
 };
 
-/**
- * @param {string} query Searched query
- */
-const searchedLocation = (query) => updateWeather(...query.split("&"));
-// updateWeather("lat=26.7951906", "lon=82.1945943")
-
+const searchedLoction = (query) => updateWeather(...query.split("&"));
 const routes = new Map([
-  ["/current-location", currentLocation],
-  ["/weather", searchedLocation],
+  ["/current-location", currentLoction],
+  ["/weather", searchedLoction],
+  ["/approximatelocation", approximatelocation],
 ]);
 
-const checkHash = function () {
+const checkHash = () => {
   const requestURL = window.location.hash.slice(1);
-
   const [route, query] = requestURL.includes
     ? requestURL.split("?")
     : [requestURL];
-
   routes.get(route) ? routes.get(route)(query) : error404();
 };
 
 window.addEventListener("hashchange", checkHash);
-window.addEventListener("load", function () {
+window.addEventListener("load", () => {
   if (!window.location.hash) {
-    window.location.hash = "#/current-location";
-  } else {
-    checkHash();
-  }
+    navigator.permissions.query({ name: "geolocation" }).then((result) => {
+      if (result.state === "granted")
+        window.location.hash = "#/current-location";
+      else approximatelocation();
+    });
+  } else checkHash();
 });
